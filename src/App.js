33f2b05337263302a3396cabf81a2a77e228b227
mac <mac@maxs-MacBook-Pro.local>
2019-05-18 12:03:59 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -11,17 +11,23 @@ import "./App.css";
 
 import Counter from "./components/Counter";
 import Todo from "./components/Todo";
+import ProductsList from "./components/products/ProductsList";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/todo">todo</Link> <Link to="/counter">counter</Link>
+        <nav>
+          <Link to="/todo">todo</Link> <Link to="/counter">counter</Link>
+          <Link to="/products">products</Link>
+        </nav>
         {/* <Route exact path="/" component={Todo} /> */}
         <Switch>
           <Route path="/todo" component={Todo} />
           <Route path="/counter" component={Counter} />
           <Redirect exact from="/" to="/todo" />
+
+          <Route path="/products" component={ProductsList} />
         </Switch>
       </Router>
     </div>
---
+++ src/components/products/EditProduct.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Editproduct extends React.Component {
+  render() {
+    return <div> EditProduct</div>;
+  }
+}
