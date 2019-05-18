87bda410253787de19365ae732032f1b1a01027a
mac <mac@maxs-MacBook-Pro.local>
2019-05-18 10:24:06 +0300

    first commit

---
+++ src/ducks/counter.test.js
@@ -0,0 +1,18 @@
+import reducer, { increase, decrease } from "./counter.js";
+
+describe("counter", () => {
+  it("increases count inside of the state", () => {
+    expect(reducer(undefined, increase())).toEqual({
+      count: 1
+    });
+  });
+});
+describe("decrease", () => {
+    describe("counter", () => {
+    it("increases count inside of the state", () => {
+      expect(reducer(undefined, decrease())).toEqual({
+        count: -1
+      });
+    });
+  });
+});
